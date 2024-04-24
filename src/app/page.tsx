ba61d63a48007c91aff2992e9815bfc6035f5d81
codeshark <jobaff03@gmail.com>
2024-04-24 12:45:43 +0000

    added force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
+const mockUrls = [
+  "https://utfs.io/f/34bb373e-0366-4375-b00e-1ad102725cec-kkfr6e.jpg",
+  "https://utfs.io/f/e90c760b-1734-42a1-9388-bc72aba78da5-97jl5t.jpg",
+  "https://utfs.io/f/512069c3-cf8b-4ed1-920c-0e238783a2a2-36pxyp.jpg",
+  "https://utfs.io/f/caa72949-0b2e-4a63-a503-8dd2ef7830ed-bce3y2.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default async function HomePage() {
-  const mockUrls = [
-    "https://utfs.io/f/34bb373e-0366-4375-b00e-1ad102725cec-kkfr6e.jpg",
-    "https://utfs.io/f/e90c760b-1734-42a1-9388-bc72aba78da5-97jl5t.jpg",
-    "https://utfs.io/f/512069c3-cf8b-4ed1-920c-0e238783a2a2-36pxyp.jpg",
-    "https://utfs.io/f/caa72949-0b2e-4a63-a503-8dd2ef7830ed-bce3y2.jpg",
-  ];
-
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
-
   const posts = await db.query.posts.findMany();
 
   return (
